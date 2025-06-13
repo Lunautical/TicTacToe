906e3e514a44aca88fba9a5027e287d2cc51ee40
Lunautical <connorjnightingale@gmail.com>
2025-06-13 09:12:26 +0800

    Adds functional game loop

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
 const gameGrid = document.getElementById("game-grid");
+const statusText = document.getElementById("status");
+const restartBtn = document.getElementById("restart");
 
 const gameBoard = new Array(9).fill(null);
 
+let gameOver;
+
+const wins = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+];
+
 gameGrid.addEventListener("click", (e) => {
-  const cell = document.getElementById(e.target.id);
+  if (gameOver) return;
+  playerTurn(e.target.id);
+  checkGameOver();
+  if (gameOver) return;
+  aiTurn();
+  checkGameOver();
+});
+
+restartBtn.addEventListener("click", () => {
+  gameOver = false;
+  gameBoard.fill(null);
+  statusText.textContent = "Game in rogress...";
+  const cells = document.getElementsByClassName("grid-item");
+  for (const cell of cells) {
+    cell.textContent = "";
+  }
+});
+
+const playerTurn = (cellID) => {
+  const cell = document.getElementById(cellID);
   if (cell.textContent !== "") return;
   cell.textContent = "X";
-  gameBoard[Number(e.target.id[e.target.id.length - 1])] = "X";
-  if (gameover()) return;
-  aiTurn();
-});
+  gameBoard[Number(cellID[cellID.length - 1])] = "X";
+};
 
 const aiTurn = () => {
   while (true) {
@@ -23,6 +55,21 @@ const aiTurn = () => {
   }
 };
 
-const gameover = () => {
-  return !gameBoard.includes(null);
+const checkGameOver = () => {
+  for (const win of wins) {
+    if (
+      gameBoard[win[0]] !== null &&
+      gameBoard[win[0]] === gameBoard[win[1]] &&
+      gameBoard[win[0]] === gameBoard[win[2]]
+    ) {
+      gameOver = true;
+      if (gameBoard[win[0]] === "X") statusText.textContent = "Player Wins!";
+      else statusText.textContent = "Computer Wins!";
+      return;
+    }
+  }
+  if (gameBoard.includes(null)) {
+    gameOver = true;
+    statusText.textContent = "Draw!";
+  }
 };
