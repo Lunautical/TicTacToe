4542e6fa501e3a7ba6e02f2afa69b6778929b3df
Lunautical <connorjnightingale@gmail.com>
2025-06-13 08:31:25 +0800

    AI for stuff and general save

---
+++ script.js
@@ -0,0 +1,24 @@
+const gameGrid = document.getElementById("game-grid");
+
+const gameBoard = new Array(9).fill(null);
+
+gameGrid.addEventListener("click", (e) => {
+  const cell = document.getElementById(e.target.id);
+  if (cell.textContent !== "") return;
+  cell.textContent = "X";
+  gameBoard[Number(e.target.id[e.target.id.length - 1])] = "X";
+  console.log(gameBoard);
+  aiTurn();
+});
+
+const aiTurn = () => {
+  while (true) {
+    const cellNum = Math.floor(Math.random() * 9);
+    const cell = document.getElementById(`grid-item-${cellNum}`);
+    if (cell.textContent === "") {
+      cell.textContent = "O";
+      gameBoard[cellNum] = "O";
+      break;
+    }
+  }
+};
